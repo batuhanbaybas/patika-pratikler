5568cce7637b61d89d673c875398ccf6228fed08
Batuhan Baybaş <b.baybas@gmail.com>
2022-03-14 22:25:16 +0300

    classes

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿Calisan calisan1 = new Calisan();
+calisan1.Ad = "Berkcan";
+calisan1.Soyad = "Gümüşışık";
+calisan1.No = 123456;
+calisan1.Departman = "Yazılım Geliştirme";
+
+calisan1.CalisanBilgiler();
+
+Calisan calisan2 = new Calisan();
+calisan2.Ad = "Şevval";
+calisan2.Soyad = "Baydemir";
+calisan2.No = 123457;
+calisan2.Departman = "Sosyal Medya";
+
+calisan2.CalisanBilgiler();
+class Calisan
+{
+    public string Ad;
+    public string Soyad;
+    public int No;
+    public string Departman;
+
+    public void CalisanBilgiler()
+    {
+        Console.WriteLine("Çalışan Adı {0}", Ad);
+        Console.WriteLine("Çalışan Soyadı {0}", Soyad);
+        Console.WriteLine("Çalışan Numarası {0}", No);
+        Console.WriteLine("Çalışan Departmanı {0}", Departman);
+    }
+}
--- Patika-Odev-2/Konsol Odev 3/Konsol Odev 3/Program.cs
+++ Patika-Odev-2/Konsol Odev 3/Konsol Odev 3/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve
+//dizinin elemanlarını sıralayan programı yazınız
+
+using System.Collections;
+
+
+Console.WriteLine("Bir cümle giriniz : ");
+string cumle = Console.ReadLine();
+ArrayList sesliHarfler = new ArrayList();
+char[] sabitSesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+char[] harfler = new char[cumle.Length];
+harfler = cumle.ToCharArray();
+foreach (var harf in harfler)
+{
+    foreach (var sabitSesli in sabitSesliHarfler)
+    {
+        if (harf == sabitSesli)
+        {
+            sesliHarfler.Add(harf);
+        }
+    }
+
+}
+
+foreach (var item in sesliHarfler)
+{
+    Console.Write(item + " ");
+}
+
